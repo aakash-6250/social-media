57b9bfc18d38fab7d903f6f8cd8647774d447ada
Aakash Patel <aakashpatel6250@gmail.com>
2024-05-19 12:59:03 +0530

    fix

--- utils/multer.js
+++ utils/multer.js
@@ -1,8 +1,6 @@
 const multer = require('multer')
 const path = require('path')
 const {v4 : uuidv4 } = require('uuid')
-const fs = require('fs')
-const fsExtra = require('fs-extra')
 
   const profile = multer.diskStorage({
     destination: function (req, file, cb) {
